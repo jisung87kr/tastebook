cc2df60699103a74cd48692aa3e35aa281aefa88
jisungyoo <jiusng87kr@gmail.com>
2022-04-26 20:24:22 +0900

    refs #15 laravel-permission 패키지 설치

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -31,5 +31,11 @@ class AuthServiceProvider extends ServiceProvider
     public function boot()
     {
         $this->registerPolicies();
+
+        Gate::before(function ($user, $ability) {
+            if ($user->hasRole('super-admin')) {
+                return true;
+            }
+        });
     }
 }
