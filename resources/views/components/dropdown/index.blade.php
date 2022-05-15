329be0e30aa574e6c8f72d92515aa81a6b8d4f69
jisungyoo <jiusng87kr@gmail.com>
2022-05-15 18:43:52 +0900

    refs #20 제트스트림 드롭다운으로 적용

--- resources/views/components/dropdown/index.blade.php
+++ resources/views/components/dropdown/index.blade.php
@@ -5,8 +5,11 @@
             <x-icons.down class="absolute right-0 top-[4px]"></x-icons.down>
         </span>
     </div>
-    <div x-show="open" class="text-left absolute bg-gray-100 rounded-lg py-3 z-50 right-0 mt-2 mx-h-6 overflow-y-auto"
-         style="display: none; max-height: 300px; word-break: keep-all">
+    <div x-show="open"
+         x-transition
+         class="text-left absolute bg-gray-100 rounded-lg py-3 z-50 right-0 mt-2 mx-h-6 overflow-y-auto"
+         style="display: none; max-height: 300px; word-break: keep-all"
+    >
         {{ $content }}
     </div>
 </div>
--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -29,20 +29,37 @@
         </div>
         <div class="ml-auto">
             @auth
-                <x-dropdown>
-                    <x-slot name="trigger">
-                        {{ auth()->user()->name }}
-                    </x-slot>
-                    <x-slot name="content">
-                        <x-dropdown.item><a href="{{route('dashboard')}}">대시보드</a></x-dropdown.item>
-                        <x-dropdown.item>
-                                <a href="{{route('logout')}}" @click.prevent="document.getElementById('form-logout').submit()">로그아웃</a>
-                                <form action="{{route('logout')}}" method="POST" id="form-logout" @logout_="alert('qwe')">
-                                    @csrf
-                                </form>
-                        </x-dropdown.item>
-                    </x-slot>
-                </x-dropdown>
+            <x-jet-dropdown align="right" width="60">
+                <x-slot name="trigger">
+                    <span class="inline-flex rounded-md">
+                        <button type="button" class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 bg-white hover:bg-gray-50 hover:text-gray-700 focus:outline-none focus:bg-gray-50 active:bg-gray-50 transition">
+                            {{ auth()->user()->name }}
+
+                            <svg class="ml-2 -mr-0.5 h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
+                                <path fill-rule="evenodd" d="M10 3a1 1 0 01.707.293l3 3a1 1 0 01-1.414 1.414L10 5.414 7.707 7.707a1 1 0 01-1.414-1.414l3-3A1 1 0 0110 3zm-3.707 9.293a1 1 0 011.414 0L10 14.586l2.293-2.293a1 1 0 011.414 1.414l-3 3a1 1 0 01-1.414 0l-3-3a1 1 0 010-1.414z" clip-rule="evenodd" />
+                            </svg>
+                        </button>
+                    </span>
+                </x-slot>
+                <x-slot name="content">
+                    <div class="w-60">
+                        <!-- Team Management -->
+                        <div class="block px-4 py-2 text-xs text-gray-400">
+                            {{ __('환영합니다') }}
+                        </div>
+                        <x-jet-dropdown-link href="{{route('dashboard')}}">
+                            {{ __('대시보드') }}
+                        </x-jet-dropdown-link>
+                        <div class="border-t border-gray-100"></div>
+                        <x-jet-dropdown-link href="{{route('logout')}}" @click.prevent="document.getElementById('form-logout').submit()">
+                            로그아웃
+                            <form action="{{route('logout')}}" method="POST" id="form-logout" @logout_="alert('qwe')">
+                                @csrf
+                            </form>
+                        </x-jet-dropdown-link>
+                    </div>
+                </x-slot>
+            </x-jet-dropdown>
             @endauth
             @guest
                 <div class="relative w-24"><a href="{{route('login')}}">로그인</a></div>
